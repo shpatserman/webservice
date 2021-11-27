b9006841bd812994056b29d037d41a7ebcaa96b1
maria.shpatserman <sokunova@gmail.com>
2021-11-27 20:26:51 +0300

    add logging

--- src/main/java/ru/netunix/webservice/WebserviceApplication.java
+++ src/main/java/ru/netunix/webservice/WebserviceApplication.java
@@ -1,15 +1,18 @@
 package ru.netunix.webservice;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-//@Ena
+@Log4j2
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
+		log.info("Simple log statement with input {}", "test message");
 	}
+
 
 }
